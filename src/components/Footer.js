3bdae006d9c23467eaefcf9088efad3569531646
RAJDIP PAL <112768747+RajdipPal19@users.noreply.github.com>
2024-04-14 23:07:14 +0530

    sec commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './styles.css'
 
 
-export class Footer extends  Component {
-    constructor(props){
+export class Footer extends Component {
+    constructor(props) {
         super(props);
     }
 
     render() {
-        return(
-<div class="footer">
-            <div className="d-flex align-items-center text-center">
-                <div className="footer-text">Copyright © 2019</div>
-                <div className="footer-text">HodlInfo.com</div>
-                <div className="footer-text">Developed By <a rel="nofollow" href="https://www.quadbtech.com" target="_blank" style={{color:"#3dc6c1"}}> QuadBTech</a></div>
-                <div class="footer-text pointer" style={{ "margin-left": "auto" }}>
-                    <a href="mailto:support@hodlinfo.com" class="footer-text-link">Support</a>
+        return (
+            <div class="footer">
+                <div className="d-flex align-items-center text-center">
+                    <div className="footer-text">Copyright © 2024</div>
+                    <div className="footer-text">HodlInfo.com</div>
+                    <div className="footer-text">Developed By Rajdip<a rel="nofollow" href="https://www.quadbtech.com" target="_blank" style={{ color: "#3dc6c1" }}> QuadBTech assignment</a></div>
+                    <div class="footer-text pointer" style={{ "margin-left": "auto" }}>
+                        <a href="mailto:support@hodlinfo.com" class="footer-text-link">Support</a>
+                    </div>
                 </div>
             </div>
-</div>
         )
     }
 }
